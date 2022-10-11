b705be6c8e12585af6dc9c7caf227c32713976a3
Elizaweta <liza_pozdn03@mail.ru>
2022-10-11 19:36:46 +0700

    файлы character

---
+++ lab_2_atd_c++/character.cpp
@@ -0,0 +1,29 @@
+#include "Character.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+Character::Character(bool friendly, int learn) {
+	this->friendly = friendly;
+	this->learn = learn;
+}
+Character::Character(int learn) {
+	this->learn;
+}
+Character::Character() {
+
+}
+Character::~Character() {
+
+}
+void Character::read() {
+	Character character;
+	cout << "Она дружелюбная? (да - 1, нет - 0)"<<endl;
+	cin >> character.friendly;
+	cout << "Оцените ее обучаемость от 1 до 5 (5 - легко обучаема):"<<endl;
+	cin >> character.learn;
+}
+void Character::display() {
+	if(character.friendly) cout << "Дружелюбная" << endl;
+	else cout << "Агрессивная" << endl;
+	cout<< "Уровень обучаемости - " << dog.character.learn << endl;
+}
---
+++ lab_2_atd_c++/character.h
@@ -0,0 +1,18 @@
+#pragma once
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+using namespace std;
+class Character {
+private:
+	bool friendly;
+	int learn;
+public:
+	Character(bool friendly, int learn);
+	Character(int learn);
+	Character();
+	~Character();
+	void read();
+	void display();
+};
